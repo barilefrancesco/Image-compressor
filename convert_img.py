b3f023f096211d2e8e5ffb5a626f5c442da53d1b
Francesco Barile <48310974+barilefrancesco@users.noreply.github.com>
2022-03-29 11:00:09 +0200

    Create convert_img.py

---
+++ convert_img.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from PIL import Image
+
+def convert_to_webp(source):
+    """Convert image to webp.
+
+    Args:
+        source (pathlib.Path): Path to source image
+
+    Returns:
+        pathlib.Path: path to new image
+    """
+    destination = source.with_suffix(".webp")
+
+    image = Image.open(source)  # Open image
+    image.save(destination, format="webp")  # Convert image to webp
+
+    return destination
+
+def main():
+    print("Inserisci il percorso della cartella (es. C:\\Users\\nomeutente\\Documents\Fotos ):")
+    path = input()
+    
+    paths = Path( path ).glob("**/*.jpg")
+    
+    for path in paths:
+        webp_path = convert_to_webp(path)
+        print(webp_path)
+
+
+main()
